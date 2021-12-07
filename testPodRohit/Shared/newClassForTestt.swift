529ce10514e6016d25134cb841f86ec350dc4dc3
rohitrna <rohitraghunath3@gmail.com>
2021-12-06 22:45:41 -0800

    Add files via upload
    test pod

---
+++ testPodRohit/Shared/newClassForTestt.swift
@@ -0,0 +1,8 @@
+//
+//  newClassForTestt.swift
+//  testPodRohit
+//
+//  Created by Rohit Nair on 12/6/21.
+//
+
+import Foundation
---
+++ testPodRohit/Shared/testPodRohitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testPodRohitApp.swift
+//  Shared
+//
+//  Created by Rohit Nair on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct testPodRohitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
